82a120cae045d0a94b7b95987ed3baab2a51cf97
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-06-23 22:13:55 +0330

    the CSS classes are modified with JS events

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const closeButton = document.querySelector(".close");
+const showButton = document.querySelectorAll(".show-modal");
+const modalWindow = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const closeModal = function () {
+  modalWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModal = function () {
+  modalWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+for (let i = 0; i < showButton.length; i++)
+  showButton[i].addEventListener("click", openModal);
+
+closeButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (param) {
+  if (param.key === "Escape") {
+    if (!modalWindow.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
